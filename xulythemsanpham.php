ce5ef871ac6fd9d8762de80893d48defd86245f4
tranduyfong <tranphong86958@gmail.com>
2024-12-22 18:12:24 +0700

    Fixed the adding products when duplicated products

--- xulythemsanpham.php
+++ xulythemsanpham.php
@@ -19,18 +19,46 @@
             if ($file['error'] === UPLOAD_ERR_OK) {
                 $fileName = basename($file['name']);
                 $targetFile = $uploadDir . $fileName;
+
                 // Lưu file vào thư mục
                 if (move_uploaded_file($file['tmp_name'], $targetFile)) {
                     $targetFile = substr($targetFile, 15);
 
-                    $sql = "INSERT INTO `sanpham`(`TenSanPham`, `GiaSanPham`, `ThongTinSanPham`, `filePath`, `LoaiSanPham`) VALUES ('$nameProduct','$costProduct','$infoProduct','$targetFile', '$typeProduct')";
-                    mysqli_query($conn, $sql);
-                    $sql_get_id = "SELECT MaSanPham FROM SanPham WHERE TenSanPham = '$nameProduct'";
-                    $result_get_id = mysqli_query($conn, $sql_get_id);
-                    $row_get_id = mysqli_fetch_assoc($result_get_id);
-                    $idProduct = $row_get_id['MaSanPham'];
-                    $sql_2 = "INSERT INTO `chitietsanpham`(`MaSanPham`, `KichCo`, `SoLuongTonKho`) VALUES ('$idProduct','$sizeProduct','$quantity')";
-                    mysqli_query($conn, $sql_2);
+                    $sql_check_product_name = "SELECT MaSanPham FROM sanpham WHERE TenSanPham = '$nameProduct'";
+                    $result_check_product_name = mysqli_query($conn, $sql_check_product_name);
+                    $variable_checked_product = mysqli_num_rows($result_check_product_name) > 0;
+                    
+                    $variable_checked_product_size = false;
+                    if ($variable_checked_product) {
+                        $row_check_product_name = mysqli_fetch_assoc($result_check_product_name);
+                        $idProduct = $row_check_product_name['MaSanPham'];
+                    
+                        $sql_check_size = "SELECT KichCo FROM chitietsanpham WHERE MaSanPham = '$idProduct' AND KichCo = '$sizeProduct'";
+                        $result_check_size = mysqli_query($conn, $sql_check_size);
+                        $variable_checked_product_size = mysqli_num_rows($result_check_size) > 0;
+                    }
+                    
+                    if ($variable_checked_product) {
+                        if ($variable_checked_product_size) {
+                            $sql_updated = "UPDATE `chitietsanpham` SET `SoLuongTonKho`='$quantityProduct' WHERE `MaSanPham` = '$idProduct' AND `KichCo` = '$sizeProduct'";
+                            mysqli_query($conn, $sql_updated);
+                        } else {
+                            $sql_add_size = "INSERT INTO `chitietsanpham`(`MaSanPham`, `KichCo`, `SoLuongTonKho`) VALUES ('$idProduct','$sizeProduct','$quantity')";
+                            mysqli_query($conn, $sql_add_size);
+                        }
+                    } else {
+                        $sql = "INSERT INTO `sanpham`(`TenSanPham`, `GiaSanPham`, `ThongTinSanPham`, `filePath`, `LoaiSanPham`) VALUES ('$nameProduct','$costProduct','$infoProduct','$targetFile', '$typeProduct')";
+                        mysqli_query($conn, $sql);
+                    
+                        $sql_get_id = "SELECT MaSanPham FROM sanpham WHERE TenSanPham = '$nameProduct'";
+                        $result_get_id = mysqli_query($conn, $sql_get_id);
+                        $row_get_id = mysqli_fetch_assoc($result_get_id);
+                        $idProduct = $row_get_id['MaSanPham'];
+                    
+                        $sql_2 = "INSERT INTO `chitietsanpham`(`MaSanPham`, `KichCo`, `SoLuongTonKho`) VALUES ('$idProduct','$sizeProduct','$quantity')";
+                        mysqli_query($conn, $sql_2);
+                    }
+                    
                 } else {
                     echo "Lỗi khi upload file.";
                 }
